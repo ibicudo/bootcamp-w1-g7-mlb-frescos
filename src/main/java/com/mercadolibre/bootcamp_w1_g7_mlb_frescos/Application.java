04b0d5dba425b8e30fc080b79a5669b7bd55b820
Lucas Mantovani <lucas.mantovani@mercadolivre.com>
2021-07-01 15:50:17 -0300

    Adding local database connection

---
+++ src/main/java/com/mercadolibre/bootcamp_w1_g7_mlb_frescos/Application.java
@@ -0,0 +1,15 @@
+package com.mercadolibre.bootcamp_w1_g7_mlb_frescos;
+
+import com.mercadolibre.bootcamp_w1_g7_mlb_frescos.config.SpringConfig;
+import com.mercadolibre.bootcamp_w1_g7_mlb_frescos.util.ScopeUtils;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		ScopeUtils.calculateScopeSuffix();
+		new SpringApplicationBuilder(SpringConfig.class).registerShutdownHook(true)
+				.run(args);
+	}
+}
