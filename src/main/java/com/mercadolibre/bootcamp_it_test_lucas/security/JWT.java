9cb8ab474960bab0766d0a0589a4318048b6b926
TonPaes <ton.paes.bccs@gmail.com>
2021-07-02 14:13:02 -0300

    WIP authentication

--- src/main/java/com/mercadolibre/bootcamp_it_test_lucas/security/JWT.java
+++ src/main/java/com/mercadolibre/bootcamp_it_test_lucas/security/JWT.java
@@ -1,15 +1,23 @@
 package com.mercadolibre.bootcamp_it_test_lucas.security;
 
+import java.io.IOException;
 import java.util.Date;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.mercadolibre.bootcamp_it_test_lucas.model.Account;
+
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.Claims;
 
 
-public class JWT {
+public class JWT extends OncePerRequestFilter {
     protected static final String secret = "test";
 
     protected static final int expirationTime = 99999;
@@ -24,6 +32,15 @@ public class JWT {
                              secret).compact();
     
     }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        if ( ! validateToken(request.getHeader("Token")) )
+            // TODO: throw a custom exception    
+            throw new IOException();
+        
+        }
 
     public static String getId(String token){
         Claims claim = decodeJWT(token);
