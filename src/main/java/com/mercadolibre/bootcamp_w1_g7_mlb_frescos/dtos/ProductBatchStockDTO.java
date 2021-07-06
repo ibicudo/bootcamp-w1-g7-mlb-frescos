cca24d165e8152c6867e38c5e8c2c6fa7e284f87
Fernando Axthelm <fernando.axthelm@mercadolivre.com>
2021-07-06 14:39:52 -0300

    response DTO

---
+++ src/main/java/com/mercadolibre/bootcamp_w1_g7_mlb_frescos/dtos/ProductBatchStockDTO.java
@@ -0,0 +1,22 @@
+package com.mercadolibre.bootcamp_w1_g7_mlb_frescos.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductBatchStockDTO {
+
+    private SectionDTO section;
+
+    private UUID productId;
+
+    private List<BatchInfoDTO> batchStock;
+}
