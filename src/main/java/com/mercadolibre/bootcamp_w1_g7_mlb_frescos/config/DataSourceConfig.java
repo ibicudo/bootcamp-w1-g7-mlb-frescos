e245cfd2026686dd15e91b8ea6f75e3af993865f
Ingrid Monalisa De Lima Bicudo <ingrid.bicudo@mercadolivre.com>
2021-07-07 09:54:17 -0300

    fix unit test

--- src/main/java/com/mercadolibre/bootcamp_w1_g7_mlb_frescos/config/DataSourceConfig.java
+++ src/main/java/com/mercadolibre/bootcamp_w1_g7_mlb_frescos/config/DataSourceConfig.java
@@ -34,30 +34,30 @@ public class DataSourceConfig {
 //                .password(FuryUtils.getEnv(password))
 //                .build();
 //    }
-//
-//    @Bean
-//    @Qualifier("datasource")
-//    @Profile("local")
-//    public DataSource getDataSourceTest(
-//            final @Value("${spring.datasource.host}") String host,
-//            final @Value("${spring.datasource.db}") String db,
-//            final @Value("${spring.datasource.username}") String user,
-//            final @Value("${spring.datasource.password}") String password
-//    ) {
-//        return DataSourceBuilder.create()
-//                .url(String.format("jdbc:mysql://%s/%s?serverTimezone=UTC", host, db))
-//                .username(user)
-//                .password(password)
-//                .build();
-//    }
-//
-//    @Bean
-//    @Qualifier("datasource")
-//    @Profile("integration_test")
-//    public DataSource getDataSourceTest() {
-//        return DataSourceBuilder.create()
-//                .url("jdbc:h2:mem:testdb")
-//                .username("sa")
-//                .build();
-//    }
+
+    @Bean
+    @Qualifier("datasource")
+    @Profile("local")
+    public DataSource getDataSourceTest(
+            final @Value("${spring.datasource.host}") String host,
+            final @Value("${spring.datasource.db}") String db,
+            final @Value("${spring.datasource.username}") String user,
+            final @Value("${spring.datasource.password}") String password
+    ) {
+        return DataSourceBuilder.create()
+                .url(String.format("jdbc:mysql://%s/%s?serverTimezone=UTC", host, db))
+                .username(user)
+                .password(password)
+                .build();
+    }
+
+    @Bean
+    @Qualifier("datasource")
+    @Profile("integration_test")
+    public DataSource getDataSourceTest() {
+        return DataSourceBuilder.create()
+                .url("jdbc:h2:mem:testdb")
+                .username("sa")
+                .build();
+    }
 }
