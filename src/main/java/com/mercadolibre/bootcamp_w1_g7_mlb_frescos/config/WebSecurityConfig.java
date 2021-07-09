9ba87f1b4e13e2f37e8e546e2adc2932081375eb
Ana Barbara Cacheado Seabra <ana.cacheado@mercadolivre.com>
2021-07-09 13:56:51 -0300

    Added permition to access swagger

--- src/main/java/com/mercadolibre/bootcamp_w1_g7_mlb_frescos/config/WebSecurityConfig.java
+++ src/main/java/com/mercadolibre/bootcamp_w1_g7_mlb_frescos/config/WebSecurityConfig.java
@@ -3,7 +3,9 @@ package com.mercadolibre.bootcamp_w1_g7_mlb_frescos.config;
 import com.mercadolibre.bootcamp_w1_g7_mlb_frescos.security.JWTFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -19,6 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/ping").permitAll()
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
+//                .mvcMatchers(HttpMethod.GET, "/swagger-ui").permitAll()
                 .mvcMatchers(HttpMethod.POST, "/inboundorder").hasRole("SUPERVISOR")
                 .mvcMatchers(HttpMethod.PUT, "/inboundorder").hasRole("SUPERVISOR")
                 .mvcMatchers(HttpMethod.GET, "/list").hasRole("SUPERVISOR")
@@ -26,4 +29,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .mvcMatchers(HttpMethod.GET, "/due-date").hasRole("SUPERVISOR")
                 .anyRequest().authenticated();
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**");
+    }
+
 }
